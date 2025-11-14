733e4b7c177a28a61a41b63602f268dab98ea516
Eliotobar <te0328032025@unab.edu.sv>
2025-11-14 12:10:57 -0600

    Actualizació7

--- hospitalmanagement/settings.py
+++ hospitalmanagement/settings.py
@@ -78,17 +78,18 @@ WSGI_APPLICATION = 'hospitalmanagement.wsgi.application'
 # -----------------------
 DATABASES = {
     'default': {
-        'ENGINE': os.environ.get('DB_ENGINE', 'django.db.backends.postgresql'),
+        'ENGINE': os.environ.get('DB_ENGINE'),
         'NAME': os.environ.get('DB_NAME'),
         'USER': os.environ.get('DB_USER'),
         'PASSWORD': os.environ.get('DB_PASSWORD'),
         'HOST': os.environ.get('DB_HOST'),
         'PORT': os.environ.get('DB_PORT', '5432'),
         'OPTIONS': {
-            'sslmode': 'require',  # obligatorio en Render
+            'sslmode': 'require',  # obligatorio para Render
         }
     }
 }
+
 
 # -----------------------
 # Validadores de contraseña
